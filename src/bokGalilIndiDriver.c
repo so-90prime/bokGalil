5b70a9ddcb1a5f4aea6e54fb3a2d8abb1f2f7166
Dan Avner <davner@email.arizona.edu>
2021-11-17 10:24:03 -0700

    had wrong array index for light

--- src/bokGalilIndiDriver.c
+++ src/bokGalilIndiDriver.c
@@ -1097,7 +1097,7 @@ void execute_ifilter_startup(ISState states[], char *names[], int n) {
           IDMessage(GALIL_DEVICE, "<ERROR> Failed calling xq_filtldm(), gstat=%d", gstat);
         }
         ifilter_startupSP.s = gstat == G_NO_ERROR ? IPS_OK : IPS_ALERT;
-        telemetry_lightsL[0].s = gstat == G_NO_ERROR ? IPS_BUSY : IPS_ALERT;
+        telemetry_lightsL[2].s = gstat == G_NO_ERROR ? IPS_BUSY : IPS_ALERT;
         IDSetLight(&telemetry_lightsLP, NULL);
 
         busy = false;
@@ -1117,7 +1117,7 @@ void execute_ifilter_startup(ISState states[], char *names[], int n) {
       }
       busy = false;
       ifilter_startupSP.s = gstat == G_NO_ERROR ? IPS_OK : IPS_ALERT;
-      telemetry_lightsL[0].s = gstat == G_NO_ERROR ? IPS_IDLE : IPS_ALERT;
+      telemetry_lightsL[2].s = gstat == G_NO_ERROR ? IPS_IDLE : IPS_ALERT;
       IDSetLight(&telemetry_lightsLP, NULL);
       ifilter_startupS[1].s = ISS_OFF;
 
