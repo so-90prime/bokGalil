0c643f16772b37f5902f080c374d2c7c2108bb67
Dan Avner <davner@email.arizona.edu>
2021-11-16 21:47:25 -0700

    i will find where to reset the switches

--- src/bokGalilIndiDriver.c
+++ src/bokGalilIndiDriver.c
@@ -1365,6 +1365,7 @@ static void execute_timer(void *p) {
     (void) sprintf(gfilter_names.filter_6, "%s", bok_gfilters[6].name);
   }
   IDMessage(GALIL_DEVICE, "guider index=%d", _gfiltn);
+  IUResetSwitch(&gfilter_changeSP);
   gfilter_changeS[_gfiltn - 1].s = ISS_ON; // Subtract one since zero based
 
   IDSetSwitch(&gfilter_changeSP, NULL);
