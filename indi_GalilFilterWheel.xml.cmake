20ec3933dbb370b7db8c40c63331ca31dbea683f
phillno <phillipnoel@arizona.edu>
2025-10-27 11:14:43 -0700

    Must use correct name for xml file

--- indi_GalilFilterWheel.xml.cmake
+++ indi_GalilFilterWheel.xml.cmake
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <driversList>
-   <devGroup group="FilterWheels">
+   <devGroup group="Filter Wheels">
       <device label="Galil FW" manufacturer="University of Arizona">
-         <driver name="BokGalil">indi_bokfw</driver>
+         <driver name="indi_GalilFilterWheel">indi_GalilFilterWheel</driver>
          <version>@CDRIVER_VERSION_MAJOR@.@CDRIVER_VERSION_MINOR@</version>
       </device>
    </devGroup>
